7587de79a4a6a395f29205c24e88fef7d90ffb39
vinceTheProgrammer <vincetheprogrammer@gmail.com>
2025-11-02 01:11:05 -0500

    Update download_gstreamer.sh

--- scripts/_prepare/ios/download_gstreamer.sh
+++ scripts/_prepare/ios/download_gstreamer.sh
@@ -1,1 +1,5 @@
-
+GST_IOS_URL="https://gstreamer.freedesktop.org/data/pkg/ios/1.26.7/gstreamer-1.0-devel-1.26.7-ios-universal.pkg"
+echo "Downloading $GST_IOS_URL..."
+curl -L "$GST_IOS_URL" -o gstreamer-ios.tar.xz
+tar -xf gstreamer-ios.tar.xz
+ls .
