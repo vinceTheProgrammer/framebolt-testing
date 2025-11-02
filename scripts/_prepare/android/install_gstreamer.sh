12097b909542a377ad794f24c50d7a6410e589b7
vincetheprogrammer <vincetheprogrammer@gmail.com>
2025-11-01 20:24:42 -0500

    fix?

--- scripts/_prepare/android/install_gstreamer.sh
+++ scripts/_prepare/android/install_gstreamer.sh
@@ -1,4 +1,11 @@
 GST_ANDROID_URL="https://github.com/vinceTheProgrammer/gstreamer-android-universal-dynamic-build/releases/download/v1.27.2.1/gstreamer-1.0-android-universal-1.27.2.1-runtime.tar.xz"
+GST_ANDROID_DEVEL_URL="https://github.com/vinceTheProgrammer/gstreamer-android-universal-dynamic-build/releases/download/v1.27.2.1/gstreamer-1.0-android-universal-1.27.2.1.tar.xz"
+
+
 echo "Downloading $GST_ANDROID_URL..."
 curl -L "$GST_ANDROID_URL" -o gstreamer-android.tar.xz
 tar -xf gstreamer-android.tar.xz
+
+echo "Downloading $GST_ANDROID_DEVEL_URL..."
+curl -L "$GST_ANDROID_DEVEL_URL" -o gstreamer-android-devel.tar.xz
+tar -xf gstreamer-android-devel.tar.xz
