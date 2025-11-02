07cbe67c5345402ba9edd4705213db1c359f7db6
vincetheprogrammer <vincetheprogrammer@gmail.com>
2025-11-01 20:40:52 -0500

    oops

--- scripts/_build/ios/build.sh
+++ scripts/_build/ios/build.sh
@@ -1,6 +1,6 @@
 cmake -S . -B build-ios -G Xcode \
                 -DCMAKE_SYSTEM_NAME=iOS \
                 -DCMAKE_OSX_SYSROOT=iphonesimulator \
-                -DCMAKE_OSX_ARCHITECTURES="x86_64" \
-                -DCMAKE_SUPPRESS_REGENERATION=ON \
+                -DCMAKE_OSX_ARCHITECTURES="x86_64"
+
 cmake --build build-ios --config Debug
