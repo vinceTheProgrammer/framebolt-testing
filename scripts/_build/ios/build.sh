f83c8f91712ac077330b5a635e0003be5c0c5497
vincetheprogrammer <vincetheprogrammer@gmail.com>
2025-11-01 20:29:46 -0500

    fix???

--- scripts/_build/ios/build.sh
+++ scripts/_build/ios/build.sh
@@ -2,4 +2,4 @@ cmake -S . -B build-ios -G Xcode \
                 -DCMAKE_SYSTEM_NAME=iOS \
                 -DCMAKE_OSX_SYSROOT=iphonesimulator \
                 -DCMAKE_OSX_ARCHITECTURES="x86_64" \
-cmake --build build-ios --config Debug
+cmake --build build-ios
