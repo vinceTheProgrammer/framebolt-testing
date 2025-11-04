789a7cdf453d82e6dc2085b7642bd02d967ec287
vinceTheProgrammer <vincetheprogrammer@gmail.com>
2025-11-03 19:06:08 -0600

    Update install_dependencies.sh

--- scripts/_prepare/linux/install_dependencies.sh
+++ scripts/_prepare/linux/install_dependencies.sh
@@ -3,6 +3,6 @@ sudo apt-get update || true
 sudo apt-get install -y \
     build-essential cmake zip git \
     libx11-dev libxext-dev libxrandr-dev libxrender-dev libxss-dev \
-    libxcursor-dev libxi-dev libxinerama-dev libwayland-dev libxkbcommon-dev \
+    libxcursor-dev libxi-dev libxinerama-dev libxtst-dev libwayland-dev libxkbcommon-dev \
     libdrm-dev libgbm-dev libasound2-dev libpulse-dev libudev-dev libdbus-1-dev \
     libibus-1.0-dev fcitx-libs-dev gstreamer1.0-dev libgstreamer-plugins-base1.0-dev
