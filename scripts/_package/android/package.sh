b50cf08356d64b7eb377f06dc308517c24d63573
vincetheprogrammer <vincetheprogrammer@gmail.com>
2025-11-03 22:24:56 -0600

    fix android artifact path

--- scripts/_package/android/package.sh
+++ scripts/_package/android/package.sh
@@ -2,3 +2,5 @@ cd android-project
 mkdir -p ../artifacts
 cp app/build/outputs/apk/release/*.apk ../artifacts/
 cd ../artifacts && zip -r framebolt-android.zip ./*
+mv framebolt-android.zip ../
+cd ..
